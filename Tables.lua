5c3451cefa28b1da635e1f7afe9ef7c7718cf54f
JavierCarrillo4 <javiercarrillo424@gmail.com>
2024-09-09 14:53:44 -0400

    Update Tables.lua

--- Tables.lua
+++ Tables.lua
@@ -1,6 +1,10 @@
 local MyTable = {"Hello!", 2, 15, true}	-- sets the 4 elements to the variable 
 										-- MyTable converting it to a table
 
+for i = 1, #MyTable do
+	print(MyTable[i])
+end
+
 print(#MyTable) -- prints the number of elements that are in the table, MyTable (4)
 
 print(MyTable[2]) -- prints the 2nd element in the table
@@ -15,7 +19,7 @@ end
 
 if table.find(MyTable, "Hello!") then	
 	local HelloIndex = table.find(MyTable, "Hello!")	-- if true, it sets the variable HelloIndex
-	table.remove(MyTable, HelloIndex)													-- to the index found in MyTable
+	table.remove(MyTable, HelloIndex)					-- to the index found in MyTable
 end
 
 table.remove(MyTable, 1)	-- removes the 1st index in the table,
@@ -24,3 +28,26 @@ table.remove(MyTable, 1)	-- removes the 1st index in the table,
 							-- (15, true, "This is an added value")
 
 print(MyTable)
+
+local menu = {	-- creates a table with 4 key-value pairs
+	["Cheeseburger"] = 15,
+	["Cola"] = 4,
+	["Pancakes"] = 8,
+	["Milk"] = 4
+}
+
+menu["Cheeseburger"] = 5	-- changes the value of the key "Cheeseburger" to 5
+
+local othermenu = {	-- another way to create a table with 4 key-value pairs
+	Cheeseburger = 15,
+	Cola = 4,
+	Pancakes = 8,
+	Milk = 4
+}
+
+print(menu["Cheeseburger"], menu["Cola"], menu["Milk"])
+
+print(othermenu.Cheeseburger, othermenu)
+
+print(menu)
+print(othermenu)
